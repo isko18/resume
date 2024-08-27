922631793e888b59aced6cfd65609627de82ea57
isko18 <toksonbaevislam2004@gmail.com>
2024-08-27 10:30:36 +0600

    resume

--- apps/settings/models.py
+++ apps/settings/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+# full_name = '' - змеинный регистр
+
+# FullName = ''  - верблюжий регистр
 # Create your models here.
+class About(models.Model):
+    photo = models.ImageField(
+        upload_to='image/',
+        verbose_name='Фото профиля'
+    )
+    fullname = models.CharField(
+        max_length=255,
+        verbose_name='ФИО'
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+    direction = models.CharField(
+        max_length=255,
+        verbose_name='Направление'
+    )
+    
+    def __str__(self) -> str:
+        return self.fullname
+    
+    class Meta:
+        verbose_name = ""
+        verbose_name_plural = "Настройки профиля"
--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from apps.settings.models import About
 
 # Create your views here.
 def index(request):
+    about = About.objects.latest('id')
     return render(request, 'index.html', locals())
